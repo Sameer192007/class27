1d070f543cb744c9fab34a6d389aa37aa89fb819
Sameer Jaiswal <sameerjaiswal917@gmail.com>
2021-02-20 17:55:49 +0530

    constrained body

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,8 +37,12 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    constraintLog=new Log(200,180,80,PI/2);
+
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body,constraintLog.body)
+    
 }
 
 function draw(){
@@ -61,4 +68,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constraintLog.display();
+    chain.display();
 }
